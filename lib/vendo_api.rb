cb353002443e84ddb20b9a4cd9bcd4e69e8c08e4
Talha Ijaz <talhaijaz123451@gmail.com>
2022-04-12 01:35:36 +0500

    Authentication

--- lib/vendo_api.rb
+++ lib/vendo_api.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "vendo_api/version"
+require "httparty"
+require_relative "vendo_api/client"
 
 module VendoApi
   class Error < StandardError; end
   # Your code goes here...
+
+  VendoApi::Client.new.perform_request
 end
--- lib/vendo_api/client.rb
+++ lib/vendo_api/client.rb
@@ -1,4 +1,11 @@
 module VendoApi
     class Client
+        def perform_request
+            url = "https://demo.getvendo.com/spree_oauth/token"
+            response = HTTParty.post(url,
+                                    basic_auth: {user_name: 'talhaijaz123451@gmail.com', password: 'vendo123', grant_type: 'password'},
+                                    headers: { 'Accept' => "application/json", 'Content-Type' => 'application/json' })
+            JSON.parse(response.body)
+        end
     end
 end
